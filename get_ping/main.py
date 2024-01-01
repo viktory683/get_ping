3237ccd8840f2e3167150193acd9898f21d3efa8
bzglve <viktory683@gmail.com>
2024-01-01 21:52:37 +0600

    docker

--- get_ping/main.py
+++ get_ping/main.py
@@ -1,7 +1,7 @@
-from subprocess import check_output
+import json
+from subprocess import check_output, Popen, PIPE
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-import jc
 from pydantic import BaseModel
 from starlette import status
 import uvicorn
@@ -15,10 +15,17 @@ class Params(BaseModel):
 
 @app.post("/getping/{ip}")
 async def getping(ip: str, params: Params):
-    out = check_output(["ping", "-c", str(params.count), ip]).decode().strip()
-    return JSONResponse(jc.parse("ping", out), status_code=status.HTTP_200_OK)
+    ping_output = Popen(["ping", "-c", str(params.count), ip], stdout=PIPE)
+    jc_output = check_output(["jc", "--ping"], stdin=ping_output.stdout)
+    ping_output.wait()
+    out = json.loads(jc_output.decode())
+    print(out)
+    return JSONResponse(out, status_code=status.HTTP_200_OK)
 
 
 def start():
-    """Launched with `poetry run start` at root level"""
     uvicorn.run("get_ping.main:app", host="0.0.0.0", port=8000, reload=True)
+
+
+if __name__ == "__main__":
+    start()
